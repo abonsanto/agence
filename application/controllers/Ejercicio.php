9a9f4ac0c4892160f99dc5f196f342d5120ff8af
Argenio Bosanto <argenio.bosanto@metamaxzone.com>
2017-10-02 09:41:12 -0400

    Color de una gráfica y activar CORS

--- application/controllers/Ejercicio.php
+++ application/controllers/Ejercicio.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Ejercicio extends CI_Controller {
 
 	public function __construct() {
+		header('Access-Control-Allow-Origin: *');
 		parent::__construct();
         $this->load->model('Consultores_model');
     }
